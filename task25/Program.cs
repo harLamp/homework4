3b8890a16e83aa67621bc64a55d7ed4f608929c1
Vladimir Kharlampidi <harlamp_83@yahoo.com>
2022-09-15 22:16:51 +0300

    Возвёл число А в натуральную степень B используя цикл

---
+++ task25/Program.cs
@@ -0,0 +1,17 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+Console.Clear();
+Console.WriteLine();
+
+int number = 3;
+int exponent = 5;
+int count = 1;
+int result = number;
+
+while (count < exponent)
+{
+    result = result * number;
+    count++;
+}
+
+Console.WriteLine($"{number} ^ {exponent} = {result}");
+Console.WriteLine();
